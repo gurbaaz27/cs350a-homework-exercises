934d9d76f641e30c1db0ad1fc64d52f9a39d26f7
gurbaaz <gurbaazsn123@gmail.com>
2022-09-04 20:28:17 +0530

    Finish up Assignment 1 : Update 4.0

--- hw1/6.hs
+++ hw1/6.hs
@@ -1,4 +1,5 @@
 -- Question 6
+-- a : using list comprehension
 a = [i | i <- [0..], i `mod` 2 == 0 || i `mod` 3 == 0 || i `mod` 5 == 0]
 
 multiplesOf :: Num t => t -> t -> [t]
@@ -9,4 +10,12 @@ stream_merge (x:xs) (y:ys) | x < y  = x:stream_merge xs (y:ys)
                            | x > y  = y:stream_merge (x:xs) ys 
                            | x == y = x:stream_merge xs ys
 
+-- b :  using self-referential streams
 b = stream_merge (stream_merge (multiplesOf 2 0)  (multiplesOf 3 0)) (multiplesOf 5 0)
+
+inf_stream x = if x `mod` 2 == 0 || x `mod` 3 == 0 || x `mod` 5 == 0
+               then x : inf_stream (x+1)
+               else inf_stream (x+1)
+
+-- alt_b : alternate way of producing stream using self-referential streams
+alt_b = inf_stream 0
--- hw1/combined.hs
+++ hw1/combined.hs
@@ -41,6 +41,7 @@ foldR f a (x:xs) = f x (foldR f a xs)
 map' :: (a -> b) -> [a] -> [b]
 map' f a = foldR (\y ys -> (f y) : ys) [] a
 -- Question 6
+-- a : using list comprehension
 a = [i | i <- [0..], i `mod` 2 == 0 || i `mod` 3 == 0 || i `mod` 5 == 0]
 
 multiplesOf :: Num t => t -> t -> [t]
@@ -51,4 +52,12 @@ stream_merge (x:xs) (y:ys) | x < y  = x:stream_merge xs (y:ys)
                            | x > y  = y:stream_merge (x:xs) ys 
                            | x == y = x:stream_merge xs ys
 
+-- b :  using self-referential streams
 b = stream_merge (stream_merge (multiplesOf 2 0)  (multiplesOf 3 0)) (multiplesOf 5 0)
+
+inf_stream x = if x `mod` 2 == 0 || x `mod` 3 == 0 || x `mod` 5 == 0
+               then x : inf_stream (x+1)
+               else inf_stream (x+1)
+
+-- alt_b : alternate way of producing stream using self-referential streams
+alt_b = inf_stream 0
