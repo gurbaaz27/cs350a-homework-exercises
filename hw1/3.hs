b3b5077087563797299f5e356ac0471efa551132
gurbaaz <gurbaazsn123@gmail.com>
2022-09-04 17:42:26 +0530

    Finish up Assignment 1 : Update 3.0

--- hw1/3.hs
+++ hw1/3.hs
@@ -3,10 +3,10 @@ merge :: Ord a => [a] -> [a] -> [a]
 merge x y = remove_duplicate (merge' x y) where
   remove_duplicate [] = []
   remove_duplicate (x:xs) = x : remove_duplicate(filter (/= x) xs)
-  
+
   merge' [] [] = []
   merge' (x:xs) [] = (x:xs)
   merge' [] (y:ys) = (y:ys)
-  merge' (x:xs) (y:ys) = if x < y 
-                         then x : merge' xs (y:ys)  
-                         else x : merge' xs ys
+  merge' (x:xs) (y:ys) | x < y  = x  : merge' xs (y:ys) 
+                       | x > y  = y  : merge' (x:xs) ys
+                       | x == y =  x : merge' xs ys  
